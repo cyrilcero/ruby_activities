20595564c54cce062b17007bb9dfc0293e825d4a
cyrilcero <cyril.cero@gmail.com>
2024-02-20 20:11:51 +0800

    fix: remove downcase method to string to maintain the input case sensitivity

--- encode.rb
+++ encode.rb
@@ -1,13 +1,13 @@
-# VOWELS = { 'a' => 1, 'e' => 2, 'i' => 3, 'o' => 4, 'u' => 5 }
 VOWELS = { a: 1, e: 2, i: 3, o: 4, u: 5 }
 
 def encode(string)
-  string.downcase.chars.map { |char| VOWELS[char.to_sym] || char }.join
+  string.chars.map { |char| VOWELS[char.to_sym] || char }.join
 end
 
 def decode(string)
-  string.downcase.chars.map { |char| VOWELS.key(char.to_i) || char }.join
+  string.chars.map { |char| VOWELS.key(char.to_i) || char }.join
 end
 
 p encode('hello')
+p encode('hEllo')
 p decode('h2ll4')
