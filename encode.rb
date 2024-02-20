3ad36c6844406e06eeddf22172d09d2fe62a31df
cyrilcero <cyril.cero@gmail.com>
2024-02-20 19:44:41 +0800

    fix: change code to accept symbols

--- encode.rb
+++ encode.rb
@@ -1,7 +1,8 @@
-VOWELS = { 'a' => 1, 'e' => 2, 'i' => 3, 'o' => 4, 'u' => 5 }
+# VOWELS = { 'a' => 1, 'e' => 2, 'i' => 3, 'o' => 4, 'u' => 5 }
+VOWELS = { a: 1, e: 2, i: 3, o: 4, u: 5 }
 
 def encode(string)
-  string.downcase.chars.map { |char| VOWELS[char] || char }.join
+  string.downcase.chars.map { |char| VOWELS[char.to_sym] || char }.join
 end
 
 def decode(string)
