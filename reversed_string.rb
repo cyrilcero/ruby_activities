03f23fb95b9b56978ccd198828d746c997f06390
cyrilcero <cyril.cero@gmail.com>
2024-03-25 20:48:25 +0800

    change the filename and method name

--- reversed_string.rb
+++ reversed_string.rb
@@ -1,8 +1,8 @@
-def reverse_a_string(string)
+def reversed_string(string)
   reversed_string = ''
   (string.length - 1).downto(0) do |item|
     reversed_string += string[item]
   end
   reversed_string
 end
-puts reverse_a_string('hello')
+puts reversed_string('hello')
