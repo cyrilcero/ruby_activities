43231949dc06c1d2524a5be260eb0a45c8998755
cyrilcero <cyril.cero@gmail.com>
2024-03-25 20:50:43 +0800

    add instructions and though process comment

--- reversed_string.rb
+++ reversed_string.rb
@@ -1,3 +1,7 @@
+# Implement reverse method that takes a string as its input and returns its reverse as the output. Avoid using built-in methods that directly solve the problem.
+
+# Iterate through the string but start from the last index down to the zeroth index.
+
 def reversed_string(string)
   reversed_string = ''
   (string.length - 1).downto(0) do |item|
@@ -5,4 +9,5 @@ def reversed_string(string)
   end
   reversed_string
 end
+
 puts reversed_string('hello')
