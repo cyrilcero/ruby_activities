28745f7fc3391e4072fa27cfc4442c5e3afeb47f
cyrilcero <cyril.cero@gmail.com>
2024-05-03 21:18:51 +0800

    add solution to reverse_alternate

---
+++ reverse_alternate.rb
@@ -0,0 +1,17 @@
+# Reverse every other word in a given string, then return the string. Throw away any leading or trailing shitespace, while ensuring there is exactly one spave between each word.
+# Punctuation marks should be treated as if they are a part of the word in this challenge.
+
+def reversed_alternate(string)
+  array_of_words = string.split
+  output = []
+  array_of_words.each_with_index do |word, index|
+    output << if index.odd?
+                word.reverse!
+              else
+                word
+              end
+  end
+  output
+end
+
+p reversed_alternate('The quick brown fox')
